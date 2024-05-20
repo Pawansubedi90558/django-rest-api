6c0d39a5f5fbf29963394de30d9a739b744b6178
pawansubedi <pawansubedi90558@gmail.com>
2024-05-20 16:10:34 +0545

    message from api

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
-endpoint="http://localhost:8000/"
+endpoint="http://localhost:8000/api/"
 
-get_response = requests.get(endpoint,json={"pawan":"subedi"})
+get_response = requests.get(endpoint,json={"a":"b"})
 
-print(get_response.text)
-print(get_response.status_code)
+# print(get_response.text)
+# print(get_response.status_code)
+print(get_response.json()["message"])
