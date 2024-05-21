f4ec91654a33701dfc5c36eced73e6bac8b28bc6
pawansubedi <pawansubedi90558@gmail.com>
2024-05-21 16:28:19 +0545

    handling query parameters

--- apple.py
+++ apple.py
@@ -1,5 +1,6 @@
 import json
 
-s="data1=data2"
+s='{"Content-Length": "24", "Content-Type": "application/json", "Host": "localhost:8000", "User-Agent": "python-requests/2.31.0", "Accept-Encoding": "gzip, deflate", "Accept": "*/*", "Connection": "keep-alive"}'
+
 data= json.loads(s)
 print(data)
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,20 @@ import json
 
 def api_home(request,*args,**kwargs):
     # print(request)
-    body = request.body
+    body = request.body #byte string of JSON data
+    values = request.GET
+    print(values)
     # print(body)
     data={}
     try:
-        data= json.loads(body)
+        data= json.loads(body) #string of JSON data -> python dict
         # print(data)
     except Exception as e:
         print(e)
+    print(request.headers)
+    data['headers'] =  dict(request.headers)
     return JsonResponse(data)
+
+"""
+{'Content-Length': '24', 'Content-Type': 'application/json', 'Host': 'localhost:8000', 'User-Agent': 'python-requests/2.31.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
+"""
