1469a895528e131eab13f313e562aae016847476
pawansubedi <pawansubedi90558@gmail.com>
2024-05-22 06:10:55 +0545

    model_to_dict from django.forms.models used

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,17 +1,21 @@
 from django.http import JsonResponse
 import json
 from products.models import Product
+from django.forms.models import model_to_dict
 
 def api_home(request,*args,**kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        #serialization is done below i.e. a model instance(model_data) is converted into python dictionary and then returned as JSON to the client
+        """serialization is done below i.e. a model instance(model_data) is converted into python dictionary and then returned as JSON to the client
         data['id'] =  model_data.id
         data['check'] = True
         data['title'] =  model_data.title
         data['content'] = model_data.content
-        data['price'] =  model_data.price
+        data['price'] =  model_data.price"""
+
+        #the above can also be done using model_to_dict method where we can specify the specific fields to be taken
+        data = model_to_dict(model_data, fields=['id','title'])
     return JsonResponse(data)
 
 """
